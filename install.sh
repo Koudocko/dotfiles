86069aad6b7a8fd75477bc4832339ecce1641ce9
Koudocko <tyler@pcquest.ca>
2022-07-02 13:49:35 -0400

    Optimized package installer.

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 # Install specified packages 
+PACKAGES=
 for package in `cat packages` 
 do
 	if ! pacman -Q $package > /dev/null 2>&1
 	then
-		echo "Package \"$package\" not installed! Prompting for install..."
-		sudo pacman -S $package
+		echo "Package \"$package\" not installed! Adding to install..."
+		PACKAGES+=" $package"	
 	fi
 done
+if ! [ -z "$PACKAGES" ] 
+then
+	sudo pacman -S $PACKAGES
+fi
 
 # Install configs for packages
 for config in *
 do
 	if [ $config != install.sh ] && [ $config != packages ]  
 	then
-		mkdir -p ~/.config/$config
-		cp -r $config/* ~/.config/$config
+		sudo mkdir -p ~/.config/$config
+		sudo cp -r $config/* ~/.config/$config/
 		echo Installed $config config
 	fi
 done
