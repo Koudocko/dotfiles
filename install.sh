02ec248adde19f32f79078c60fdd4c68ffd69e86
Koudocko <tyler@pcquest.ca>
2022-07-04 13:47:03 -0400

    Re-implemented AUR support.

--- install.sh
+++ install.sh
@@ -1,30 +1,48 @@
 #!/bin/sh
 
 # Install specified pacman packages 
-echo INSTALLING PACMAN PACKAGES
+echo ======INSTALLING PACMAN PACKAGES=====
 PACKAGES=
-for package in `cat packages`
+for package in `cat pac_packages`
 do
     if ! pacman -Q $package > /dev/null 2>&1; then
         if pacman -Qg $package > /dev/null 2>&1; then
-            choice=N
-            read -p "Do you want to install $package group? (y/N): " choice
+            read -p "Do you want to install $package group? (y/N): " choice || choice=N
             if [ "$choice" == "y" ]; then
-                echo "[+] Group "$package" added to install..."
+                echo "[+] Group ($package) added to install..."
                 PACKAGES+=" $package"
             fi
         else
-            echo "[+] Package "$package" not installed! Adding to install..."
+            echo "[+] Package ($package) not installed! Adding to install..."
             PACKAGES+=" $package"
         fi
     else
-        echo "[.] Package "$package" installed!"
+        echo "[.] Package ($package) installed!"
     fi
 done
 [ ! -z "$PACKAGES" ] && sudo pacman -S $PACKAGES 
+PACKAGES=
+
+# Install yay package
+read -p "Do you want to install AUR packages? (y/N)" choice || choice=N
+if [ "$choice" == "y" ]; then
+	echo =====INSTALLING AUR PACKAGES=====
+	! pacman -Q yay > /dev/null 2>&1 && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+	for package in `cat aur_packages`
+	do
+	    if ! pacman -Qm $package > /dev/null 2>&1; then
+			echo "[+] Package ($package) not installed! Adding to install..."
+            PACKAGES+=" $package"
+		else
+			echo "[.] Package ($package) installed!"
+		fi
+	done
+	[ ! -z "$PACKAGES" ] && yay -S $PACKAGES
+fi
 
 # Install configs for packages
-echo INSTALLING PACKAGE CONFIGS 
+echo =====INSTALLING PACKAGE CONFIGS======
 for config in configs/.*
 do
     if [ $config != configs/.. ] && [ $config != configs/. ]; then
