bb8a20ce18533e9536c30f5d9a906c6f5b37c0c0
Koudocko <tyler@pcquest.ca>
2022-09-18 18:06:07 -0400

    Added script to install gtk theming.

---
+++ configs/gtk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p ~/.local/share/themes
+git clone https://github.com/UnnatShaneshwar/AtomOneDarkTheme.git && cp -r AtomOneDarkTheme ~/.local/share/themes/ && rm -r AtomOneDarkTheme
---
+++ scripts/gtk.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo mkdir -p ~/.local/share/themes
+git clone https://github.com/UnnatShaneshwar/AtomOneDarkTheme.git && sudo cp -r AtomOneDarkTheme ~/.local/share/themes/ && sudo rm -r AtomOneDarkTheme
