6416e4384f5ad65c7a116cccb30d75fca07f6d3e
Koudocko <tyler@pcquest.ca>
2023-04-28 13:51:04 -0400

    Added nitrogen script. Added image clipping command.

---
+++ scripts/nitrogen.sh
@@ -0,0 +1,17 @@
+echo "[geometry]
+posx=9
+posy=48
+sizex=1900
+sizey=1021
+
+[nitrogen]
+view=icon
+recurse=true
+sort=alpha
+icon_caps=false
+dirs=$HOME/Pictures/Wallpapers;" > "$HOME/.config/nitrogen/nitrogen.cfg"
+
+echo "[xin_-1]
+file=$HOME/Pictures/Wallpapers/ArchWallpaper0.png
+mode=0
+bgcolor=#000000" > "$HOME/.config/nitrogen/bg-saved.cfg"
