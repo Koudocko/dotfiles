1a0f26d2f78068f2ae2f7be0452028696f3871de
Koudocko <tyler@pcquest.ca>
2023-06-13 22:26:04 -0400

    Volume control changes.

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-! pacman -Qm vim-plug > /dev/null 2>&1 && vim +'PlugInstall --sync' +qa && vim +'CocInstall -sync coc-rust-analyzer coc-pairs coc-json coc-clangd coc-java coc-html' +qall
+pacman -Qm vim-plug > /dev/null 2>&1 && vim +'PlugInstall --sync' +qa && vim +'CocInstall -sync coc-rust-analyzer coc-pairs coc-json coc-clangd coc-java coc-html' +qall
