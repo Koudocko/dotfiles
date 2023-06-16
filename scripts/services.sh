f6b39bdbbc257b9db00e98f66283a1353c526853
Koudocko <tyler@pcquest.ca>
2023-06-16 17:44:46 -0400

    Essential services. Reflector.

--- scripts/services.sh
+++ scripts/services.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-sudo systemctl enable NetworkManager; sudo systemctl enable sshd; sudo systemctl enable systemd-timesyncd; sudo systemctl enable paccache.timer
+sudo systemctl enable NetworkManager
+sudo systemctl enable sshd
+sudo systemctl enable systemd-timesyncd
+
+sudo systemctl enable paccache.timer
+sudo systemctl enable fstrim.timer
+sudo systemctl enable reflector.timer
