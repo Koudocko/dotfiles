7b4b36566ded5dd6e955756b0e7994c1b750fbac
Koudocko <tyler@pcquest.ca>
2023-10-03 21:20:59 -0400

    After 1 year... I finally thought, huh, using printers would be nice huh.

--- scripts/services.sh
+++ scripts/services.sh
@@ -3,6 +3,8 @@
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable systemd-timesyncd.service
 sudo systemctl enable ufw.service
+sudo systemctl enable cups.service
+sudo systemctl enable avahi-daemon.service
 
 sudo systemctl enable paccache.timer
 sudo systemctl enable fstrim.timer
